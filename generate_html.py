02b03363e4a7b3146fa4577e3c377d1871647a14
Arpa Roy <arparoycollection@gmail.com>
2025-06-17 19:18:39 +0600

    Update generate_html.py

--- generate_html.py
+++ generate_html.py
@@ -1,12 +1,12 @@
 import os
 
-def convert_md_table_to_html(md_file, html_file, css_path="assets/style.css"):
+def convert_md_table_to_html(md_file, html_file, css_path="style.css"):
     with open(md_file, 'r', encoding='utf-8') as f:
         lines = f.readlines()
 
     table_lines = [line.strip() for line in lines if "|" in line]
     if not table_lines or len(table_lines) < 3:
-        print("❌ No valid Markdown table found.")
+        print("No valid Markdown table found.")
         return
 
     headers = [h.strip() for h in table_lines[0].split("|")[1:-1]]
