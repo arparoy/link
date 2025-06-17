4c396d7be412c5ef3703d9fc06170fbd9fbd648f
Arpa Roy <arparoycollection@gmail.com>
2025-06-17 17:03:33 +0600

    Update generate_links.py

--- generate_links.py
+++ generate_links.py
@@ -1,8 +1,32 @@
 import hashlib
+import re
 
 LINK_FILE = 'link.txt'
 OUTPUT_FILE = 'links.md'
 HASH_FILE = '.link_hash'
+
+PLATFORM_PATTERNS = {
+    "YouTube": r"(youtube\.com|youtu\.be)",
+    "Facebook": r"facebook\.com",
+    "Google Drive": r"drive\.google\.com",
+    "Google Photos": r"photos\.google\.com",
+    "Terabox": r"terabox\.com",
+    "OneDrive": r"1drv\.ms|onedrive\.live\.com",
+    "Mega": r"mega\.nz",
+    "Dropbox": r"dropbox\.com",
+    "Proton Drive": r"proton\.me|protondrive\.com",
+    "Blog": r"\.blogspot\.com|\.wordpress\.com",
+    "Instagram": r"instagram\.com",
+    "TikTok": r"tiktok\.com",
+    "Twitter": r"twitter\.com|x\.com",
+    "Telegram": r"t\.me",
+}
+
+def detect_platform(link):
+    for platform, pattern in PLATFORM_PATTERNS.items():
+        if re.search(pattern, link, re.IGNORECASE):
+            return platform
+    return "Other"
 
 def hash_links(links):
     return hashlib.sha256("".join(links).encode()).hexdigest()
@@ -25,10 +49,11 @@ def save_hash(new_hash):
 def generate_md(links):
     with open(OUTPUT_FILE, 'w', encoding='utf-8') as f:
         f.write("# Links Table\n\n")
-        f.write("| Serial Number | Link |\n")
-        f.write("|---------------|------|\n")
+        f.write("| Serial Number | Link | Platform |\n")
+        f.write("|---------------|------|----------|\n")
         for i, link in enumerate(links, 1):
-            f.write(f"| {i} | [Click here]({link}) |\n")
+            platform = detect_platform(link)
+            f.write(f"| {i} | [Click here]({link}) | {platform} |\n")
 
 def main():
     links = read_links()
